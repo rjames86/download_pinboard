cb22be7b116bfb6d67e5d077eecf2f17cc67f0a5
Martijn Engler <git@martijnengler.nl>
2014-12-30 20:16:19 +0100

    fix for very long filenames (255+ chars) on OS X (HFS+)

--- lib/download_pinboard.py
+++ lib/download_pinboard.py
@@ -97,7 +97,10 @@ class PinboardDownloader:
         self.set_last_updated()
 
     def _clean_filename(self, description):
-        return _SAVE_PATH + re.sub(r'[/]', ' ', description) + '.webloc'
+        cleaned_filename = re.sub(r'[/]', ' ', description)
+        if(len(cleaned_filename) > (248)):
+            cleaned_filename = cleaned_filename[0:248]
+        return _SAVE_PATH + cleaned_filename + '.webloc'
 
     def _get_pinboard_session(self, username, password, token):
         # todo: should maybe try attempting to connect a few times
